63d2b0b21aad86a711650615d659c8cb76e56f10
richardlee-kr <richardl22245@gmail.com>
2024-12-03 14:19:26 +0900

    [CHORE] Save Scene

--- PianoMotionRhythmGame/Assets/RhythmGame/01.Script/RhythmGame/LaneManager.cs
+++ PianoMotionRhythmGame/Assets/RhythmGame/01.Script/RhythmGame/LaneManager.cs
@@ -21,15 +21,15 @@ namespace RhythmGame
 
         private void Update()
         {
-            if(Input.GetKey(KeyCode.A))
+            if(Input.GetKey(KeyCode.S))
             {
                 Hit(0);
             }
-            if(Input.GetKey(KeyCode.S))
+            if(Input.GetKey(KeyCode.D))
             {
                 Hit(1);
             }
-            if(Input.GetKey(KeyCode.D))
+            if(Input.GetKey(KeyCode.F))
             {
                 Hit(2);
             }
